2beaca205300a44e7976b27f43cf094c9f55a547
Wichakool <s6302041520139@email.kmutnb.ac.th>
2021-08-05 17:10:03 +0700

    new comment

--- dart/week2.dart
+++ dart/week2.dart
@@ -1,20 +1,24 @@
-void main() {
+void main() { //----------เรียกทำงาน------------>
   //print(callGen(1991));
   //var x = callColor("white");
   //print(x);
   callWeb("google",protocal: "https",port: 443);
-}
-//เรียก function
+
+  
+}//----------สิ้นสุด Main ------------>
+//เรียก function                 <------short-hand-------->
 String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
 
+//<for in(foreach)>    
 int callColor([String newColor = "pink"]) {
   List<dynamic> color = ["red", "blue", "green"];
 
   for (var item in color) {
     print(item);
   }
-  return color.length;
+  return color.length;//return จำนวนข้อมูลกลับ
 }
 
+//arrow function เป็นแบบ optinal ทั้ง position และ name
 void callWeb(String web ,{int port = 80, String protocal = "http"}) => 
 print("$protocal $web : $port");
